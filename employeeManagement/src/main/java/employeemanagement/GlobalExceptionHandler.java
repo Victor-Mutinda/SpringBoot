3f7be3ddc01e302310ad8ad049f64b09e3961dd2
victor mutinda <victormutinda01@gmail.com>
2026-09-22 23:14:42 +0300

    Exceptions touch ups

--- employeeManagement/src/main/java/employeemanagement/GlobalExceptionHandler.java
+++ employeeManagement/src/main/java/employeemanagement/GlobalExceptionHandler.java
@@ -6,19 +6,23 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.*;
+import java.util.Map;
+import java.util.LinkedHashMap;
 
 
 @RestControllerAdvice // Tells spring to handle exceptions Globally across all REST controllers.
 public class GlobalExceptionHandler {
     
-    @ExceptionHandler (MethodArgumentNotValidException.class)
+    @ExceptionHandler (MethodArgumentNotValidException.class) // Tells Spring to immediately execute
+    // the method below whenever MethodArgumentNotValidException occurs.
 
     public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex){
         Map<String,String> errors = new LinkedHashMap<>();
 
         ex.getBindingResult().getAllErrors().forEach((error) -> {
+// getBindingResult() - gets info associated with failed request.
+// getAllErrors() - gets all validation errors.     
+// FieldError is a class that lets you access field details       
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
